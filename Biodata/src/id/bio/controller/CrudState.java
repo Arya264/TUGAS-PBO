42f9f732c8a047bc5773679744ed1577fa618b2d
Arya264 <134837370+Arya264@users.noreply.github.com>
2024-05-26 07:16:50 +0630

    Add files via upload

---
+++ Biodata/src/id/bio/controller/CrudState.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.bio.controller;
+
+/**
+ * @author 13020220078
+ * Nama : Muh.Yusran
+ * Kelas : A2
+ */
+public class CrudState {
+    INSERT, UPDATE, DELETE, QUERY
+}
---
+++ Biodata/src/id/bio/dao/JurusanDAO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.bio.dao;
+
+/**
+ * @author 13020220078
+ * Nama : Muh.Yusran
+ * Kelas : A2
+ */
+
+import id.bio.model. Jurusan;
+import java.util.List;
+
+public interface JurusanDAO{
+    public boolean insert(Jurusan j);
+    public boolean update(Jurusan j);
+    public boolean delete(Jurusan j);
+    public List<Jurusan> getAllJurusan ();
+    public Jurusan getByID(String id);
+    public List<Jurusan> getByName(String name);
+}
